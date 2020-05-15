5c34d0b59d434171937b0d39c41b2f09bae5b0f5
dabin <820040739@qq.com>
2020-05-15 18:46:36 +0800

    main模块

--- module-home/src/main/java/com/dabin/module_home/HomeFragment.java
+++ module-home/src/main/java/com/dabin/module_home/HomeFragment.java
@@ -1,17 +1,13 @@
 package com.dabin.module_home;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.dabin.library_base.fragment.MvvmBaseFragment;
 import com.dabin.library_base.fragment.MvvmLazyFragment;
 import com.dabin.library_base.viewmodel.IMvvmBaseViewModel;
 import com.dabin.library_common.router.RouterFragmentPath;
 import com.dabin.module_home.databinding.HomeFragmentHomeBinding;
+
 @Route(path = RouterFragmentPath.Home.PAGER_HOME)
-public class HomeFragment extends MvvmLazyFragment<HomeFragmentHomeBinding,IMvvmBaseViewModel> {
+public class HomeFragment extends MvvmLazyFragment<HomeFragmentHomeBinding, IMvvmBaseViewModel> {
 
     @Override
     protected void onFragmentFirstVisible() {
